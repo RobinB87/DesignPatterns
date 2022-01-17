46ae5155a9d7de62599ae972d0fe3370e30ded27
Robin Bette <robinbette@hotmail.com>
2022-01-17 11:55:58 +0100

    Add various (fake) shipping providers

---
+++ Strategy_UseDifferentShippingProviders/Business/Strategies/Shipping/SwedishPostalServiceShippingStrategy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using Strategy_UseDifferentShippingProviders.Business.Models;
+
+namespace Strategy_UseDifferentShippingProviders.Business.Strategies.Shipping
+{
+    public class SwedishPostalServiceShippingStrategy : IShippingStrategy
+    {
+        public void Ship(Order order)
+        {
+            using (var client = new HttpClient())
+            {
+                /// TODO: Implement PostNord Shipping Integration
+                
+                Console.WriteLine("Order is shipped with PostNord");
+            }
+        }
+    }
+}
