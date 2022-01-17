c20c7c7d48689bf9259169244e31fe933d8ec90b
Robin Bette <robinbette@hotmail.com>
2022-01-17 10:57:24 +0100

    Strategy pattern: add create an invoice starter project

---
+++ Strategy_CreateAnInvoice/Business/Strategies/SalesTax/ISalesTaxStrategy.cs
@@ -0,0 +1,9 @@
+﻿using Strategy_CreateAnInvoice.Business.Models;
+
+namespace Strategy_CreateAnInvoice.Business.Strategies.SalesTax
+{
+    public interface ISalesTaxStrategy
+    {
+        public decimal GetTaxFor(Order order);
+    }
+}
---
+++ Strategy_CreateAnInvoice/Program.cs
@@ -0,0 +1,26 @@
+﻿using Strategy_CreateAnInvoice.Business.Models;
+using Strategy_CreateAnInvoice.Business.Strategies.SalesTax;
+using System;
+
+namespace Strategy_CreateAnInvoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order
+            {
+                ShippingDetails = new ShippingDetails 
+                { 
+                    OriginCountry = "Sweden",
+                    DestinationCountry = "Sweden"
+                },
+                SalesTaxStrategy = new SwedenSalesTaxStrategy()
+            };
+            
+            order.LineItems.Add(new Item("CSHARP_SMORGASBORD", "C# Smorgasbord", 100m, ItemType.Literature), 1);
+
+            Console.WriteLine(order.GetTax());
+        }
+    }
+}
