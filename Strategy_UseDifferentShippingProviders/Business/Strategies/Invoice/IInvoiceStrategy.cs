2fa1f9886349e84abaa54f5337b04eb3307eb364
Robin Bette <robinbette@hotmail.com>
2022-01-17 11:52:25 +0100

    Strategy_UseDifferentShippingProviders: Set up starting proj

---
+++ Strategy_UseDifferentShippingProviders/Business/Strategies/Invoice/IInvoiceStrategy.cs
@@ -0,0 +1,9 @@
+﻿using Strategy_UseDifferentShippingProviders.Business.Models;
+
+namespace Strategy_UseDifferentShippingProviders.Business.Strategies.Invoice
+{
+    public interface IInvoiceStrategy
+    {
+        public void Generate(Order order);
+    }
+}
---
+++ Strategy_UseDifferentShippingProviders/Business/Strategies/SalesTax/ISalesTaxStrategy.cs
@@ -0,0 +1,9 @@
+﻿using Strategy_UseDifferentShippingProviders.Business.Models;
+
+namespace Strategy_UseDifferentShippingProviders.Business.Strategies.SalesTax
+{
+    public interface ISalesTaxStrategy
+    {
+        public decimal GetTaxFor(Order order);
+    }
+}
