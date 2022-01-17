2a42627c03903f68031780eca596044ea5a7af97
Robin Bette <robinbette@hotmail.com>
2022-01-17 11:19:40 +0100

    Add various services to leverage for example streamwriter, smtpclient (emailstrategy) and printondemand, which leverages an api

---
+++ Strategy_CreateAnInvoice/Business/Strategies/Invoice/FileInvoiceStrategy.cs
@@ -0,0 +1,16 @@
+﻿using Strategy_CreateAnInvoice.Business.Models;
+using System;
+using System.IO;
+
+namespace Strategy_CreateAnInvoice.Business.Strategies.Invoice
+{
+    public override void Generate(Order order)
+    {
+        using (var stream = new StreamWriter($"invoice_{Guid.NewGuid()}.txt"))
+        {
+            stream.Write(GenerateTextInvoice(order));
+
+            stream.Flush();
+        }
+    }
+}
---
+++ Strategy_CreateAnInvoice/Business/Strategies/Invoice/PrintOnDemandInvoiceStrategy.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Strategy_CreateAnInvoice.Business.Models;
+using System;
+using System.Net.Http;
+
+namespace Strategy_CreateAnInvoice.Business.Strategies.Invoice
+{
+    // Here, not leverage the abstract class, but the interface  itself,
+    // because we are not going to get a text representation to pass to this print on demand service
+    //
+    // The used API is purely fictional
+    public class PrintOnDemandInvoiceStrategy : IInvoiceStrategy
+    {
+        public void Generate(Order order)
+        {
+            using (var client = new HttpClient())
+            {
+                var content = JsonConvert.SerializeObject(order);
+
+                client.BaseAddress = new Uri("https://pluralsight.com");
+
+                client.PostAsync("/print-on-demand", new StringContent(content));
+            }
+        }
+    }
+}
