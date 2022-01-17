3d2aa3b8979b98c6f5a438fefe33c3037c66bb4f
Robin Bette <robinbette@hotmail.com>
2022-01-17 11:12:22 +0100

    Add email invoice strategy

---
+++ Strategy_CreateAnInvoice/Business/Strategies/Invoice/EmailInvoiceStrategy.cs
@@ -0,0 +1,29 @@
+﻿using Strategy_CreateAnInvoice.Business.Models;
+using System.Net;
+using System.Net.Mail;
+
+namespace Strategy_CreateAnInvoice.Business.Strategies.Invoice
+{
+    public class EmailInvoiceStrategy : InvoiceStrategy
+    {
+        public override void Generate(Order order)
+        {
+            var body = GenerateTextInvoice(order);
+
+            // Actual implementation is not important here, just showcase
+            using (SmtpClient client = new SmtpClient("smtp.sendgrid.net", 587))
+            {
+                NetworkCredential credentials = new NetworkCredential("USERNAME", "PASSWORD");
+                client.Credentials = credentials;
+
+                MailMessage mail = new MailMessage("YOUR EMAIL", "YOUR EMAIL")
+                {
+                    Subject = "We've created an invoice for your order",
+                    Body = GenerateTextInvoice(order)
+                };
+
+                client.Send(mail);
+            }
+        }
+    }
+}
