40131b8e49033314db7814a5a77c4df09f89e7a6
Robin Bette <robinbette@hotmail.com>
2022-01-17 10:16:12 +0100

    * Fix starter project

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,7 +1,7 @@
-﻿using Strategy_Pattern_First_Look.Business.Models;
+﻿using Strategy.Business.Models;
 using System;
 
-namespace Strategy_Pattern_First_Look
+namespace Strategy
 {
     class Program
     {
