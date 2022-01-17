96427f2b37c80b8ad58196519c190f07bacd544e
Robin Bette <robinbette@hotmail.com>
2022-01-17 11:12:11 +0100

    Set up interface and abstract method

---
+++ Strategy_CreateAnInvoice/Business/Strategies/Invoice/IInvoiceStrategy.cs
@@ -0,0 +1,9 @@
+﻿using Strategy_CreateAnInvoice.Business.Models;
+
+namespace Strategy_CreateAnInvoice.Business.Strategies.Invoice
+{
+    interface IInvoiceStrategy
+    {
+        public void Generate(Order order);
+    }
+}
---
+++ Strategy_CreateAnInvoice/Business/Strategies/Invoice/InvoiceStrategy.cs
@@ -0,0 +1,36 @@
+﻿using Strategy_CreateAnInvoice.Business.Models;
+using System;
+
+namespace Strategy_CreateAnInvoice.Business.Strategies.Invoice
+{
+    /// <summary>
+    /// Whoever is inheriting from our invoice strategy, needs to implement the generate method;
+    /// hence make it abstract
+    /// </summary>
+    public abstract class InvoiceStrategy : IInvoiceStrategy
+    {
+        public abstract void Generate(Order order);
+
+        public string GenerateTextInvoice(Order order)
+        {
+            var invoice = $"INVOICE DATE: {DateTimeOffset.Now}{Environment.NewLine}";
+
+            invoice += $"ID|NAME|PRICE|QUANTITY{Environment.NewLine}";
+
+            foreach (var item in order.LineItems)
+            {
+                invoice += $"{item.Key.Id}|{item.Key.Name}|{item.Key.Price}|{item.Value}{Environment.NewLine}";
+            }
+
+            invoice += Environment.NewLine + Environment.NewLine;
+
+            var tax = order.GetTax();
+            var total = order.TotalPrice + tax;
+
+            invoice += $"TAX TOTAL: {tax}{Environment.NewLine}";
+            invoice += $"TOTAL: {total}{Environment.NewLine}";
+
+            return invoice;
+        }
+    }
+}
