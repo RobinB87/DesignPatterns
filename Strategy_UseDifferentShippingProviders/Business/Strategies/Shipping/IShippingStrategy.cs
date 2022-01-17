fe585221f401c8602d8a5d690ca5b6f0005273f4
Robin Bette <robinbette@hotmail.com>
2022-01-17 11:55:06 +0100

    Add interface

---
+++ Strategy_UseDifferentShippingProviders/Business/Strategies/Shipping/IShippingStrategy.cs
@@ -0,0 +1,9 @@
+﻿using Strategy_UseDifferentShippingProviders.Business.Models;
+
+namespace Strategy_UseDifferentShippingProviders.Business.Strategies.Shipping
+{
+    public interface IShippingStrategy
+    {
+        void Ship(Order order);
+    }
+}
